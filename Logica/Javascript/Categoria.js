99bf743faaa384e35102ee4fad053cb0123f9fed
HugoTalledos <47650545+HugoTalledos@users.noreply.github.com>
2019-03-20 23:19:27 -0500

    Categoría - método eliminar

--- Logica/Javascript/Categoria.js
+++ Logica/Javascript/Categoria.js
@@ -31,3 +31,20 @@ function editar(){
 	};
 	xhr.send(info);
 }
+
+
+function eliminar(){
+	var idCat=document.getElementById("idCat");
+	var url="/ProyectoBases2/Logica/Entidades/Categoria.php"
+	var text_idCat= idCat.value;
+	var info="opcion=eliminar&idCat="+text_idCat;
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST",url,true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4 && xhr.status == 200){
+			alert(xhr.responseText);
+		}
+	};
+	xhr.send(info);
+}
